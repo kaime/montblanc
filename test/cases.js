00fcb049a4994e569228186c0e983bf5748043b9
Jaume Alemany <jaume@krokis.com>
2014-02-12 01:22:39 +0100

    Some test refactoring

--- test/cases.js
+++ test/cases.js
@@ -1,27 +1,35 @@
 var stylus = require('stylus'),
     montblanc = require('../'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path'),
+    glob = require('glob');
 
-var cases = fs.readdirSync('test/cases').filter(function(file){
-  return ~file.indexOf('.styl');
-}).map(function(file){
-  return file.replace('.styl', '');
-});
 
-describe('test', function(){
-  cases.forEach(function(test){
-    var name = test.replace(/[-.]/g, ' ');
-    it(name, function(){
-      var path = 'test/cases/' + test + '.styl';
-      var styl = fs.readFileSync(path, 'utf8').replace(/\r/g, '');
-      var css = fs.readFileSync('test/cases/' + test + '.css', 'utf8').replace(/\r/g, '').trim();
+var readFile = function(file) {
+ return fs.readFileSync(file, 'utf8').replace(/\r/g, '');
+}
+
+var tests = glob.sync('test/cases/*.styl');
+
+describe('Test cases', function() {
+
+  tests.forEach(function(file) {
+
+    var name = path.basename(file, '.styl');
+
+    it(name, function() {
+
+      var styl = readFile(file, 'utf8'),
+          css = readFile(path.join(path.dirname(file), name + '.css')).trim();
 
       var style = stylus(styl)
           .use(montblanc())
-          .set('filename', path);
+          .set('filename', file);
 
       style.render(function(err, actual){
-        if (err) throw err;
+        if (err) {
+          throw err;
+        }
         actual.trim().should.equal(css);
       });
     })
