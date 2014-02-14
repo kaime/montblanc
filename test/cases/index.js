c138ca24dad70cb038155aa98569088e530ea820
Jaume Alemany <jaume@krokis.com>
2014-02-14 22:32:41 +0100

    Removed superfluous tests from cases

--- test/cases/index.js
+++ test/cases/index.js
@@ -2,16 +2,28 @@ var stylus = require('stylus'),
     montblanc = require('../../'),
     fs = require('fs'),
     path = require('path'),
-    glob = require('glob');
+    glob = require('glob'),
+    browsers = require('../../src/support/browsers.json');
 
 
 var readFile = function(file) {
  return fs.readFileSync(file, 'utf8').replace(/\r/g, '');
 }
 
-var tests = glob.sync('test/cases/*.styl');
-
 describe('Test cases', function() {
+
+  // Reset all `support-to-*` vars on every case to avoid prefixes here.
+  var reset = [];
+
+  for (var browser in browsers) {
+    reset.push(
+      'support-for-' + browser + ' = no-support'
+    );
+  }
+
+  reset = reset.join("\n")
+
+  var tests = glob.sync('test/cases/*.styl');
 
   tests.forEach(function(file) {
 
@@ -19,7 +31,7 @@ describe('Test cases', function() {
 
     it(name, function() {
 
-      var styl = readFile(file, 'utf8'),
+      var styl = reset + "\n" + readFile(file, 'utf8'),
           css = readFile(path.join(path.dirname(file), name + '.css')).trim();
 
       var style = stylus(styl)
