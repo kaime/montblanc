793a93e618f0c1f443801bf24b9cdd197e41e2b3
Jaume Alemany <jaume@krokis.com>
2014-02-15 00:16:47 +0100

    New test case runner, inspired by @jasonkuhrt's stylus-test-runner

--- test/cases/index.js
+++ test/cases/index.js
@@ -10,6 +10,36 @@ var readFile = function(file) {
  return fs.readFileSync(file, 'utf8').replace(/\r/g, '');
 }
 
+var getDirective = function(str) {
+  if ('//' === str.trim().substr(0, 2)) {
+    var comment = str.substr(2).trim();
+    if ('@' === comment.substr(0, 1)) {
+      var w = comment.indexOf(' ');
+
+      if (w > 0) {
+        return comment.substr(1, w).trim()
+      } else {
+        return comment.substr(1);
+      }
+
+    }
+  }
+  return false;
+}
+
+var getDirectiveComment = function(str) {
+  if ('//' === str.trim().substr(0, 2)) {
+    var comment = str.substr(2).trim();
+    if ('@' === comment.substr(0, 1)) {
+      var w = comment.indexOf(' ', 1);
+      if (w > 0) {
+        return comment.substr(w).trim();
+      }
+    }
+  }
+  return '';
+}
+
 describe('Cases', function() {
 
   // Reset all `support-to-*` vars on every case to avoid prefixes here.
@@ -27,23 +57,117 @@ describe('Cases', function() {
 
   tests.forEach(function(file) {
 
-    var name = path.basename(file, '.styl');
+    var name = path.basename(file, '.styl'),
+        expected = null,
+        them = [];
 
-    it(name, function() {
+    var lines = readFile(file, 'utf8').split(/\r?\n/);
 
-      var styl = reset + "\n" + readFile(file, 'utf8'),
-          css = readFile(path.join(path.dirname(file), name + '.css')).trim();
+    for (var line, dir, i = 0, l = lines.length; i < l; i++) {
 
-      var style = stylus(styl)
-          .use(montblanc())
-          .set('filename', file);
+      line = lines[i].trim();
 
-      style.render(function(err, actual){
-        if (err) {
-          throw err;
+      if (line === '') continue;
+
+      if (false !== (dir = getDirective(line))) {
+        if ('describe' === dir) {
+          name = getDirectiveComment(line);
+          i++;
         }
-        actual.trim().should.equal(css);
+      }
+      break;
+    }
+
+    for (; i < l; i++) {
+
+      line = lines[i];
+
+      if (line === '') continue;
+
+      if (false !== (dir = getDirective(line))) {
+
+        if ('it' === dir) {
+
+          var desc = getDirectiveComment(line);
+              styl = [],
+              expected = [];
+
+          i++;
+
+          while (i < l) {
+
+            line = lines[i];
+
+            if (false !== (dir = getDirective(line))) {
+
+              if ('expect' === dir) {
+
+                ++i;
+
+                while (i <= l) {
+
+                  if (i === l || (false !== getDirective(lines[i]))) {
+
+                    if (expected.length <= 0 || styl.length <= 0) {
+                      throw "Empty case :(";
+                    }
+
+                    them.push([desc, function() {
+                      var styl = reset + "\n" + this.styl,
+                          expected = this.expected;
+
+                      var style = stylus(styl)
+                          .use(montblanc())
+                          .set('filename', this.file);
+
+                      style.render(function(err, actual){
+                        if (err) {
+                          throw err;
+                        }
+                        actual.trim().should.equal(expected.trim());
+                      });
+
+                    }.bind({
+                      expected: expected.join("\n"),
+                      styl: styl.join("\n"),
+                      file: file
+                    })]);
+                    i--;
+                    break;
+
+                  } else {
+                    expected.push(lines[i]);
+                  }
+
+                  i++;
+                }
+
+              } else {
+                throw ":(";
+              }
+
+            } else {
+              styl.push(line);
+            }
+
+            i++;
+          }
+
+        } else {
+          throw ":( @ " + file;
+        }
+      }
+    }
+
+    if (them.length < 1) {
+      throw "No cases found in file " + file;
+    }
+
+    describe(name, function() {
+      them.forEach(function(one) {
+        it(one[0], one[1]);
       });
     })
+
   });
 })
