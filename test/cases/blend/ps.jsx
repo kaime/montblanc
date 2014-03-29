aeaf76c818543f00518db110473c17f50fc4eb69
Jaume Alemany <jaume@krokis.com>
2014-03-29 21:40:37 +0100

    And moar

--- test/cases/blend/ps.jsx
+++ test/cases/blend/ps.jsx
@@ -472,8 +472,8 @@ app.displayDialogs = DialogModes.NO;
 
 // Create the doc. We only need 1px.
 var doc = app.documents.add(1, 1);
-doc.bitsPerChannel = BitsPerChannelType.EIGHT;
-//doc.bitsPerChannel = BitsPerChannelType.SIXTEEN;
+//doc.bitsPerChannel = BitsPerChannelType.EIGHT;
+doc.bitsPerChannel = BitsPerChannelType.SIXTEEN;
 //doc.bitsPerChannel = BitsPerChannelType.THIRTYTWO;
 
 // Add a color sampler to query result color
