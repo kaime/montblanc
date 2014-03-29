002c1465836d8adba16be78d2e1acecac1614f3e
Jaume Alemany <jaume@krokis.com>
2014-03-29 21:25:45 +0100

    More WIP

--- test/cases/blend/ps.jsx
+++ test/cases/blend/ps.jsx
@@ -24,8 +24,6 @@ if (!Array.prototype.map) {
 
   Array.prototype.map = function(fun /*, thisArg */) {
 
-    "use strict";
-
     if (this === void 0 || this === null)
       throw new TypeError();
 
@@ -98,10 +96,10 @@ function CSSColor(r, g, b, a) {
 
   if (typeof a === 'undedined') a = 1;
 
-  this.red = Math.round(parseFloat(r));
-  this.green = Math.round(parseFloat(g));
-  this.blue = Math.round(parseFloat(b));
-  this.alpha = Math.round(parseFloat(a));
+  this.red = parseFloat(r);
+  this.green = parseFloat(g);
+  this.blue = parseFloat(b);
+  this.alpha = parseFloat(a);
 }
 
 /**
@@ -112,7 +110,8 @@ CSSColor.parseRGBA = (function() {
   var regRGBA = /^\s*rgba?\s*\(\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*(?:,\s*([0-9\.]+\s*(%?)))?\s*\)\s*$/i;
 
   return function(rgba) {
-    var m = rgba.match(regRGBA)
+
+    var m = rgba.match(regRGBA);
 
     if (m) {
 
@@ -161,18 +160,221 @@ CSSColor.parseHex = (function() {
 
 })()
 
+
 /**
- * Parses a rgb(a) or hexadecimal CSS color.
+ * Parses a CSS named color.
+ */
+CSSColor.parseNamed = function(color) {
+
+    var colors = {
+     "aliceblue": "#f0f8ff",
+     "antiquewhite": "#faebd7",
+     "aqua": "#00ffff",
+     "aquamarine": "#7fffd4",
+     "azure": "#f0ffff",
+     "beige": "#f5f5dc",
+     "bisque": "#ffe4c4",
+     "black": "#000000",
+     "blanchedalmond": "#ffebcd",
+     "blue": "#0000ff",
+     "blueviolet": "#8a2be2",
+     "brown": "#a52a2a",
+     "burlywood": "#deb887",
+     "cadetblue": "#5f9ea0",
+     "chartreuse": "#7fff00",
+     "chocolate": "#d2691e",
+     "coral": "#ff7f50",
+     "cornflowerblue": "#6495ed",
+     "cornsilk": "#fff8dc",
+     "crimson": "#dc143c",
+     "cyan": "#00ffff",
+     "darkblue": "#00008b",
+     "darkcyan": "#008b8b",
+     "darkgoldenrod": "#b8860b",
+     "darkgray": "#a9a9a9",
+     "darkgrey": "#a9a9a9",
+     "darkgreen": "#006400",
+     "darkkhaki": "#bdb76b",
+     "darkmagenta": "#8b008b",
+     "darkolivegreen": "#556b2f",
+     "darkorange": "#ff8c00",
+     "darkorchid": "#9932cc",
+     "darkred": "#8b0000",
+     "darksalmon": "#e9967a",
+     "darkseagreen": "#8fbc8f",
+     "darkslateblue": "#483d8b",
+     "darkslategray": "#2f4f4f",
+     "darkslategrey": "#2f4f4f",
+     "darkturquoise": "#00ced1",
+     "darkviolet": "#9400d3",
+     "deeppink": "#ff1493",
+     "deepskyblue": "#00bfff",
+     "dimgray": "#696969",
+     "dimgrey": "#696969",
+     "dodgerblue": "#1e90ff",
+     "firebrick": "#b22222",
+     "floralwhite": "#fffaf0",
+     "forestgreen": "#228b22",
+     "fuchsia": "#ff00ff",
+     "gainsboro": "#dcdcdc",
+     "ghostwhite": "#f8f8ff",
+     "gold": "#ffd700",
+     "goldenrod": "#daa520",
+     "gray": "#808080",
+     "grey": "#808080",
+     "green": "#008000",
+     "greenyellow": "#adff2f",
+     "honeydew": "#f0fff0",
+     "hotpink": "#ff69b4",
+     "indianred ": "#cd5c5c",
+     "indigo ": "#4b0082",
+     "ivory": "#fffff0",
+     "khaki": "#f0e68c",
+     "lavender": "#e6e6fa",
+     "lavenderblush": "#fff0f5",
+     "lawngreen": "#7cfc00",
+     "lemonchiffon": "#fffacd",
+     "lightblue": "#add8e6",
+     "lightcoral": "#f08080",
+     "lightcyan": "#e0ffff",
+     "lightgoldenrodyellow": "#fafad2",
+     "lightgrey": "#d3d3d3",
+     "lightgreen": "#90ee90",
+     "lightpink": "#ffb6c1",
+     "lightsalmon": "#ffa07a",
+     "lightseagreen": "#20b2aa",
+     "lightskyblue": "#87cefa",
+     "lightslategray": "#778899",
+     "lightslategrey": "#778899",
+     "lightsteelblue": "#b0c4de",
+     "lightyellow": "#ffffe0",
+     "lime": "#00ff00",
+     "limegreen": "#32cd32",
+     "linen": "#faf0e6",
+     "magenta": "#ff00ff",
+     "maroon": "#800000",
+     "mediumaquamarine": "#66cdaa",
+     "mediumblue": "#0000cd",
+     "mediumorchid": "#ba55d3",
+     "mediumpurple": "#9370d8",
+     "mediumseagreen": "#3cb371",
+     "mediumslateblue": "#7b68ee",
+     "mediumspringgreen": "#00fa9a",
+     "mediumturquoise": "#48d1cc",
+     "mediumvioletred": "#c71585",
+     "midnightblue": "#191970",
+     "mintcream": "#f5fffa",
+     "mistyrose": "#ffe4e1",
+     "moccasin": "#ffe4b5",
+     "navajowhite": "#ffdead",
+     "navy": "#000080",
+     "oldlace": "#fdf5e6",
+     "olive": "#808000",
+     "olivedrab": "#6b8e23",
+     "orange": "#ffa500",
+     "orangered": "#ff4500",
+     "orchid": "#da70d6",
+     "palegoldenrod": "#eee8aa",
+     "palegreen": "#98fb98",
+     "paleturquoise": "#afeeee",
+     "palevioletred": "#d87093",
+     "papayawhip": "#ffefd5",
+     "peachpuff": "#ffdab9",
+     "peru": "#cd853f",
+     "pink": "#ffc0cb",
+     "plum": "#dda0dd",
+     "powderblue": "#b0e0e6",
+     "purple": "#800080",
+     "red": "#ff0000",
+     "rosybrown": "#bc8f8f",
+     "royalblue": "#4169e1",
+     "saddlebrown": "#8b4513",
+     "salmon": "#fa8072",
+     "sandybrown": "#f4a460",
+     "seagreen": "#2e8b57",
+     "seashell": "#fff5ee",
+     "sienna": "#a0522d",
+     "silver": "#c0c0c0",
+     "skyblue": "#87ceeb",
+     "slateblue": "#6a5acd",
+     "slategrey": "#708090",
+     "slategrey": "#708090",
+     "snow": "#fffafa",
+     "springgreen": "#00ff7f",
+     "steelblue": "#4682b4",
+     "tan": "#d2b48c",
+     "teal": "#008080",
+     "thistle": "#d8bfd8",
+     "tomato": "#ff6347",
+     "turquoise": "#40e0d0",
+     "violet": "#ee82ee",
+     "wheat": "#f5deb3",
+     "white": "#ffffff",
+     "whitesmoke": "#f5f5f5",
+     "yellow": "#ffff00",
+     "yellowgreen": "#9acd32"
+  };
+
+  color = color.toLowerCase();
+
+  if (color in colors) {
+    return CSSColor.parseHex(colors[color]);
+  }
+
+  return null;
+}
+
+/**
+ * Parses a `grey()` color.
+ */
+CSSColor.parseGrey = (function() {
+
+  var regGrey = /^\s*gr[ea]y\s*\(([0-9\.]+)(%?)\s*(?:,([0-9\.]+)(%?))?\s*\)\s*$/i
+
+  return function(color) {
+
+    var m = color.match(regGrey);
+
+    if (m) {
+
+      var g = parseFloat(m[1]);
+
+      if (m[2] === '%') {
+        g /= 100;
+      }
+
+      g = 1 - g;
+
+      var alpha = 1;
+
+      if (typeof m[3] !== 'undefined') {
+        alpha = parseFloat(m[3]);
+
+        if (m[4] === '%') {
+          alpha /= 100;
+        }
+      }
+
+      return new CSSColor(
+        g * 255, g * 255, g * 255, alpha
+      );
+    }
+  }
+
+})();
+
+/**
+ * Parses a named, rgb(a) or hexadecimal CSS color.
  */
 CSSColor.parse = function(color) {
-  return CSSColor.parseRGBA(color) || CSSColor.parseHex(color);
+  return CSSColor.parseNamed(color) || CSSColor.parseGrey(color) || CSSColor.parseRGBA(color) || CSSColor.parseHex(color);
 }
 
 /**
  * Creates a new `CSSColor` from a PS `solidColor` and an optional `alpha`
  * value.
  */
-CSSColor.fromPSColor = function(color, alpha) {
+CSSColor.fromPS = function(color, alpha) {
   if (typeof alpha === 'undefined') alpha = 1;
 
   return new CSSColor(
@@ -186,7 +388,7 @@ CSSColor.fromPSColor = function(color, alpha) {
 /**
  * Returns equivalent (opaque) PS `solidColor`.
  */
-CSSColor.prototype.toPSColor = function() {
+CSSColor.prototype.toPS = function() {
   var color = new SolidColor;
 
   color.rgb.red = this.red;
@@ -197,16 +399,21 @@ CSSColor.prototype.toPSColor = function() {
 }
 
 CSSColor.prototype.toString = function() {
+
+  var r = Math.round(this.red),
+      g = Math.round(this.green),
+      b = Math.round(this.blue);
+
   if (this.alpha < 1) {
     return 'rgba(' +
-      this.red +
-      ', ' + this.green +
-      ', ' + this.blue +
+      r +
+      ', ' + g +
+      ', ' + b +
       ', ' + this.alpha +
     ')';
   }
 
-  var hex = [this.red, this.green, this.blue].map(
+  var hex = [r, g, b].map(
     function(c) {
       return ('0' + c.toString(16).toLowerCase()).substr(-2);
     }
@@ -245,11 +452,12 @@ function readJSON(path) {
 }
 
 // Read samples from JSON file.
-var colors = readJSON('./samples.json');
+var samples = readJSON('./samples.json');
 
 // Actual stylus code.
 var code = [
-  '// @describe `blend()`'
+  '// @describe `blend()`',
+  ''
 ];
 
 // Keep old Photoshop settings...
@@ -266,6 +474,7 @@ app.displayDialogs = DialogModes.NO;
 var doc = app.documents.add(1, 1);
 doc.bitsPerChannel = BitsPerChannelType.EIGHT;
 //doc.bitsPerChannel = BitsPerChannelType.SIXTEEN;
+//doc.bitsPerChannel = BitsPerChannelType.THIRTYTWO;
 
 // Add a color sampler to query result color
 var sampler = doc.colorSamplers.add([0, 0]);
@@ -280,69 +489,82 @@ var sourceLayer = doc.artLayers.add();
 doc.selection.selectAll();
 
 // Blend each color with each other.
-var mode, i, j, l = colors.length, source, result, expect;
+var mode, i, j, l = samples.length, source, result, styl, css;
 
 for (mode in MODES) {
 
-  // This is the Stylus part of the case.
-  code.push(
-    '// @it `' + mode + '`',
-    'body'
-  );
+  try {
 
-  // This is the CSS part of the case after `@expect`.
-  expect = [
-    'body {'
-  ];
+    // This blend mode could not be available in working color mode
+    sourceLayer.blendMode = MODES[mode];
 
-  sourceLayer.blendMode = MODES[mode];
+    styl = [];
 
-  for (i = 0; i < l; i++) {
+    // This is the Stylus part of the case after `@it`
+    styl.push(
+      'body'
+    );
 
-    // The backdrop color.
-    backdrop = CSSColor.parse(colors[i]);
+    // This is the CSS part of the case after `@expect`
+    css = [
+      'body {'
+    ];
 
-    // Apply to backdrop layer.
-    doc.activeLayer = backdropLayer;
-    doc.selection.fill(backdrop.toPSColor());
+    for (i = 0; i < l; i++) {
 
-    for (j = 0; j < l; j++) {
+      // The backdrop color.
+      backdrop = CSSColor.parse(samples[i]);
 
-      // The source color.
-      source = CSSColor.parse(colors[j]);
+      // Apply to backdrop layer.
+      doc.activeLayer = backdropLayer;
+      doc.selection.fill(backdrop.toPS());
 
-      // Apply to source layer.
-      doc.activeLayer = sourceLayer;
-      doc.selection.fill(source.toPSColor());
-      sourceLayer.opacity = source.alpha * 100;
+      for (j = 0; j < l; j++) {
 
-      // Collect computed result.
-      result = CSSColor.fromPSColor(sampler.color);
+        try {
+          // The source color.
+          source = CSSColor.parse(samples[j]);
 
-      $.writeln(mode + ': ' , source, ' + ', backdrop, ' = ', result);
+          // Apply to source layer.
+          doc.activeLayer = sourceLayer;
+          doc.selection.fill(source.toPS());
+          sourceLayer.opacity = source.alpha * 100;
 
-      // Append to Stylus code.
-      code.push(
-        "  color: blend('" + mode + "', " + colors[j] + ', ' + colors[i] + ')'
-      );
+          // Collect computed result.
+          result = CSSColor.fromPS(sampler.color);
 
-      // Append to expected CSS code
-      expect.push(
-        '  color: ' + result + ';'
-      );
+          //$.writeln(mode + ': ' , source, ' + ', backdrop, ' = ', result);
+        } catch (e) {
+          continue;
+        }
+
+        // Append to Stylus code.
+        styl.push(
+          "  color: blend('" + mode + "', " + samples[j] + ', ' + samples[i] + ')'
+        );
+
+        // Append to expected CSS code
+        css.push(
+          '  color: ' + result + ';'
+        );
+      }
     }
+
+    // Finish case code
+    css.push(
+      '}'
+    );
+
+  } catch (e) {
+    continue;
   }
 
-  // Finish case code
-  expect.push(
-    '}'
+  code = code.concat(
+    ['', '// @it `' + mode + '`'],
+    styl,
+    ['', '// @expect'],
+    css
   );
-
-  code = code.concat([
-    '',
-    '// @expect',
-    ''
-  ], expect);
 }
 
 // Almost done. Restore preferences now.
